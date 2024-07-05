d8e1d7ca55ca075d40510fdb56a88515cc367152
Sivalakshmi-C <csivalakshmi9@gmail.com>
2024-07-05 12:13:35 +0530

    javascript

---
+++ Navbar.js
@@ -0,0 +1,17 @@
+const Navbar = () => {
+    return (  
+        <div className="navbar">
+            <div>
+           
+                <a href="#">Home</a>
+                <a href="#">About us</a>
+                <a href="#">Contact us</a>
+            </div>
+            <div className="content">
+
+            </div>
+       </div>
+    );
+}
+ 
+export default Navbar;
